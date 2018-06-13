bbe72fb2510a6cb228cef139071d3a2511e36d3f
Matthieu Lenogue <matthieu.lenogue@gmail.com>
2018-06-13 17:56:49 +0200

    Fichier events
    Mise en place d'un fichier d'événements

--- js/events.js
+++ js/events.js
@@ -1,29 +1,10 @@
-var events_heatmap = new L.TileLayer.HeatCanvas({},{
-	'step':0.5, 
-	'degree':HeatCanvas.LINEAR, 
-	'opacity':0.7
-});
-
 var data = [
-	[protectorate_justitian.lat, protectorate_justitian.lng, 103], 
-	[protectorate_spital.lat, protectorate_spital.lng, 86], 
-	[protectorate_ignatz.lat, protectorate_ignatz.lng, 41], 
-	[protectorate_ferropolis.lat, protectorate_ferropolis.lon, 40], 
+	[protectorate_justitian.lat, protectorate_justitian.lng, 0.1], 
+	[protectorate_spital.lat, protectorate_spital.lng, 0.2], 
+	[protectorate_ignatz.lat, protectorate_ignatz.lng, 0.5], 
+	[protectorate_ferropolis.lat, protectorate_ferropolis.lng, 0.8], 
 ];
-for(var i=0,l=data.length; i<l; i++) {
-	events_heatmap.pushData(data[i][0], data[i][1], data[i][2]);
-	// if(data[i][2]> 20) {
-		// var marker = new L.Marker(new L.LatLng(data[i][0], data[i][1]));
-		// marker.bindPopup(data[i].toString());
-		// markerLayers.addLayer(marker);
-	// }
-}
 
-events_heatmap.onRenderingStart(function(){
-	document.getElementById("status").innerHTML = 'rendering';
-});
-events_heatmap.onRenderingEnd(function(){
-	document.getElementById("status").innerHTML = '';
-});
+var events_heatmap = new L.heatLayer(data, {max: 1});
 
 var events_layer = L.layerGroup([events_heatmap]);
