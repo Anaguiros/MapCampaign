bb4929e91bc956fd63020c35e3872ba7a22cad85
Matthieu Lenogue <matthieu.lenogue@gmail.com>
2018-10-12 12:03:38 +0200

    Page statique

--- js/pnj.js
+++ js/pnj.js
@@ -1,6 +1,6 @@
 // Create the icon used for the player markers. May be changed to any image you wish.
 var playerIcon = L.icon({
-    iconUrl: '../img/players.png',
+    iconUrl: 'img/players.png',
     iconSize: [38, 38],
 });
 
@@ -8,7 +8,7 @@ var playerIcon = L.icon({
 var redPNJPopup = "<div><b>name :</b> Robert</br><b>description :</b> Super PNJ qui va finir la campagne vivant</div>";
 
 // Creates the player markers, sets the latitude and longitude. Passes options for custom icon and custom popup values.
-var redPNJ_protectorate = L.marker(protectorate_justitian, {icon: L.icon({iconUrl: '../img/players_RED.png',iconSize: [38, 38],}), pmIgnore: true}).bindPopup(redPNJPopup);
+var redPNJ_protectorate = L.marker(protectorate_justitian, {icon: L.icon({iconUrl: 'img/players_RED.png',iconSize: [38, 38],}), pmIgnore: true}).bindPopup(redPNJPopup);
 
 // Creates a group of player markers to be added to layer control later. Adds player markers to map.
 var pnjs_protectorate = L.layerGroup([redPNJ_protectorate]);
--- js/stage1.js
+++ js/stage1.js
@@ -3,7 +3,7 @@ var voyage_animate_marker = L.Marker.movingMarker([protectorate_siege,protectora
 voyage_animate_marker.addStation(1, 5000);
 voyage_animate_marker.addStation(2, 5000);
 
-voyage_animate_marker.setIcon(L.icon({iconUrl: '../img/players_GROUP.png',iconSize: [38, 38],}))
+voyage_animate_marker.setIcon(L.icon({iconUrl: 'img/players_GROUP.png',iconSize: [38, 38],}))
 
 voyage_animate_marker.on('end', function() {
     voyage_animate_marker.bindPopup('<b>Hop on déclenche un event END !</b>', {closeOnClick: false})
